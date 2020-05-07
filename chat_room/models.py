b82cd38afd519ba2ce163dbe2d23d4a3eef67353
Prateek Juneja <prateek.juneja@impressico.com>
2020-05-07 19:30:07 +0530

    id , image added in Room Model and member is changed to members with manytomany field

--- chat_room/models.py
+++ chat_room/models.py
@@ -6,10 +6,12 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Room(models.Model):
+    id = models.UUIDField(auto_add=True, primary_key=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
-    member = models.ForeignKey(User, on_delete=models.CASCADE, related_name='member')
+    members = models.ManyToManyField(User)
     name = models.CharField(max_length=1200)
-    timestamp = models.DateTimeField(auto_now_add=True, primary_key=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    room_image = models.ImageField()
 
     def __str__(self):
         return self.name
